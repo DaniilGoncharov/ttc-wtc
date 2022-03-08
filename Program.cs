7e235b1f5aaf3d5ccd0e035bd80b1826a06a794d
Daniil Goncharov <daniilgoncharov08@gmail.com>
2022-03-08 16:57:59 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ttc_wtc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
